6f109508cdad8ebe75ad1c6ef21cd18214f7f5a9
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-02-16 10:49:20 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ReadFromFileGUI
+{
+    partial class ReadFromFileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbersListBox = new System.Windows.Forms.ListBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numbersListBox
+            // 
+            this.numbersListBox.FormattingEnabled = true;
+            this.numbersListBox.Location = new System.Drawing.Point(14, 12);
+            this.numbersListBox.Name = "numbersListBox";
+            this.numbersListBox.Size = new System.Drawing.Size(158, 420);
+            this.numbersListBox.TabIndex = 0;
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(178, 12);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 1;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // ReadFromFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 447);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.numbersListBox);
+            this.Name = "ReadFromFileForm";
+            this.Text = "Read From File";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox numbersListBox;
+        private System.Windows.Forms.Button readButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Read From File GUI
+ * Pawelski
+ * 2/15/2022
+ * This program reads numbers from a file and displays them in a list box.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadFromFileGUI
+{
+    public partial class ReadFromFileForm : Form
+    {
+        public ReadFromFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            // Please add teh appropriate path below!
+            const string PATH_FILE = @"";
+            FileStream file = new FileStream(PATH_FILE, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            while(!reader.EndOfStream)
+            {
+                numbersListBox.Items.Add(reader.ReadLine());
+            }
+            reader.Close();
+            file.Close();
+        }
+    }
+}
